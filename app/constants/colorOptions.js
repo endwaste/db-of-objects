7802e57d701712633bb2e06e06ee590126eb23c6
Barbara Zoani <barbara@endwaste.io>
2025-03-04 18:54:13 -0800

    order options in alphabetical order

--- app/constants/colorOptions.js
+++ app/constants/colorOptions.js
@@ -1,19 +1,18 @@
 const colorOptions = [
+  { value: "black", label: "Black" },
+  { value: "blue", label: "Blue" },
+  { value: "brown", label: "Brown" },
+  { value: "gold", label: "Gold" },
+  { value: "gray", label: "Gray" },
+  { value: "green", label: "Green" },
+  { value: "orange", label: "Orange" },
+  { value: "pink", label: "Pink" },
+  { value: "purple", label: "Purple" },
+  { value: "red", label: "Red" },
+  { value: "tan", label: "Tan" },
   { value: "transparent", label: "Transparent" },
   { value: "translucent", label: "Translucent" },
   { value: "white", label: "White" },
-  { value: "gray", label: "Gray" },
-  { value: "black", label: "Black" },
-  { value: "purple", label: "Purple" },
-  { value: "pink", label: "Pink" },
-  { value: "orange", label: "Orange" },
-  { value: "yellow", label: "Yellow" },
-  { value: "green", label: "Green" },
-  { value: "blue", label: "Blue" },
-  { value: "red", label: "Red" },
-  { value: "tan", label: "Tan" },
-  { value: "brown", label: "Brown" },
-  { value: "gold", label: "Gold" }
+  { value: "yellow", label: "Yellow" }
 ];
-
 export default colorOptions;
